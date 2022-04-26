6afb6c5fd1645d9cc8869a8c344ffb24bb17b8ff
ruiqingluo <ruiqingluo1@163.com>
2022-04-26 19:22:52 +0800

    # hw02: ddpg arguments

--- Homework_2/argument.py
+++ Homework_2/argument.py
@@ -26,7 +26,7 @@ def dqn_arguments(parser):
     # Self_defined arguments
     # For epsilon-greedy liner decay
     parser.add_argument("--start_e", default=1, type=float)
-    parser.add_argument("--end-e", default=0.01, type=float)
+    parser.add_argument("--end_e", default=0.01, type=float)
     parser.add_argument("--exploration_fraction", default=0.4, type=float)
     # Off-policy learning begin step
     parser.add_argument("--learning_starts", default=10000, type=int)
@@ -56,3 +56,20 @@ def pg_arguments(parser):
     parser.add_argument("--n_frames", default=int(30000), type=int)
 
     return parser
+
+
+def ddpg_arguments(parser):
+    parser.add_argument("--seed", default=11037, type=int)
+    parser.add_argument("--use_cuda", default=True, type=bool)
+    parser.add_argument('--env_name', default="LunarLanderContinuous-v2", help='environment name')
+    parser.add_argument("--total_timesteps", type=int, default=1000000)
+    parser.add_argument("--learning_rate", type=float, default=3e-4)
+    parser.add_argument("--buffer_size", type=int, default=int(1e6))
+    parser.add_argument("--gamma", type=float, default=0.99)
+    parser.add_argument("--tau", type=float, default=0.005)
+    parser.add_argument("--max_grad_norm", type=float, default=0.5)
+    parser.add_argument("--batch_size", type=int, default=256)
+    parser.add_argument("--exploration_noise", type=float, default=0.1)
+    parser.add_argument("--learning_starts", type=int, default=25e3)
+    parser.add_argument("--policy_frequency", type=int, default=2)
+    parser.add_argument("--noise_clip", type=float, default=0.5)
